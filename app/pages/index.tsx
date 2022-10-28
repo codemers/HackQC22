a20f5805c632b47d8bfdf0212bb4726ec6012950
Charles-Olivier Demers <charles-olivierdemers@hotmail.com>
2022-10-28 17:53:31 -0400

    Add base project

---
+++ app/pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>HackQC 2022</h1>
+    </div>
+  );
+}
