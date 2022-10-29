b0754b9dfc306d4d9039247c895120bf6ebd0202
Dereck Pouliot <dereckmpouliot@gmail.com>
2022-10-29 14:11:42 -0400

    Finalise profile icons and buttons

--- app/pages/app/profile/profile.tsx
+++ app/pages/app/profile/profile.tsx
@@ -1,5 +1,9 @@
+/* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
 import Authenticated from "../../../layout";
+import { InboxIcon, CreditCardIcon, ArrowPathIcon, ClockIcon, 
+  GiftIcon, UserCircleIcon, TruckIcon, Cog6ToothIcon, 
+  QuestionMarkCircleIcon, InformationCircleIcon, ArrowRightOnRectangleIcon} from "@heroicons/react/20/solid";
 
 import AccountButton from "../../../components/AccountButton/AccountButton";
 import AccountButtonGroup from "../../../components/AccountButtonGroup/AccountButtonGroup";
@@ -13,40 +17,38 @@ export default function Profile() {
       </p>
 
       <AccountButtonGroup>
-        <AccountButton title="Messagerie" link="/app/messages" icon="/avatar-3.jpg"/>
+        <AccountButton title="Messagerie" link="/app/messages" icon={<InboxIcon className="w-4 h-4 inline-block" />}/>
       </AccountButtonGroup>
 
       <AccountButtonGroup>
-        <AccountButton title="Méthodes de paiement" link="/app/payment" icon="/avatar-3.jpg"/>
-        <AccountButton title="Paiements auto." link="/app/payment-auto" icon="/avatar-3.jpg"/>
-        <AccountButton title="Historique des transactions" link="/app/transactions" icon="/avatar-3.jpg"/>
+        <AccountButton title="Méthodes de paiement" link="/app/payment" icon={<CreditCardIcon className="w-4 h-4 inline-block" />}/>
+        <AccountButton title="Paiements auto." link="/app/payment-auto" icon={<ArrowPathIcon className="w-4 h-4 inline-block" />}/>
+        <AccountButton title="Historique des transactions" link="/app/transactions" icon={<ClockIcon className="w-4 h-4 inline-block" />}/>
       </AccountButtonGroup>
       
       <AccountButtonGroup>
-        <AccountButton title="Offrir un cadeau" link="/app/gift" icon="/avatar-3.jpg"/>
-        <AccountButton title="Saisir un code recu" link="/app/gift-code" icon="/avatar-3.jpg"/>
+        <AccountButton title="Offrir un cadeau" link="/app/gift" icon={<GiftIcon className="w-4 h-4 inline-block" />}/>
+        <AccountButton title="Saisir un code recu" link="/app/gift-code" icon={<GiftIcon className="w-4 h-4 inline-block" />}/>
       </AccountButtonGroup>
 
       <AccountButtonGroup>
-        <AccountButton title="Carte de membre" link="/app/transactions" icon="/avatar-3.jpg"/>
+        <AccountButton title="Carte de membre" link="/app/transactions" icon={<CreditCardIcon className="w-4 h-4 inline-block" />}/>
       </AccountButtonGroup>
       
       <AccountButtonGroup>
-        <AccountButton title="Mon compte" link="/app/my-account" icon="/avatar-3.jpg"/>
-        <AccountButton title="Mes véhicules" link="/app/my-vehicles" icon="/avatar-3.jpg"/>
-        <AccountButton title="Mes bornes" link="/app/my-terminal" icon="/avatar-3.jpg"/>
-        <AccountButton title="Préférences" link="/app/settings" icon="/avatar-3.jpg"/>
+        <AccountButton title="Mon compte" link="/app/my-account" icon={<UserCircleIcon className="w-4 h-4 inline-block" />}/>
+        <AccountButton title="Mes véhicules" link="/app/my-vehicles" icon={<TruckIcon className="w-4 h-4 inline-block" />}/>
+        <AccountButton title="Mes bornes" link="/app/me" icon={<TruckIcon className="w-4 h-4 inline-block" />}/>
+        <AccountButton title="Préférences" link="/app/settings" icon={<Cog6ToothIcon className="w-4 h-4 inline-block" />}/>
       </AccountButtonGroup>
       
       <AccountButtonGroup>
-        <AccountButton title="Boite à outils - FAQ" link="/app/faq" icon="/avatar-3.jpg"/>
-        <AccountButton title="À propos" link="/app/about" icon="/avatar-3.jpg"/>
+        <AccountButton title="Boite à outils - FAQ" link="/app/faq" icon={<QuestionMarkCircleIcon className="w-4 h-4 inline-block" />}/>
+        <AccountButton title="À propos" link="/app/about" icon={<InformationCircleIcon className="w-4 h-4 inline-block" />}/>
       </AccountButtonGroup>
 
       <AccountButtonGroup>
-        <AccountButton title="Admin" link="/app/me" icon="/avatar-3.jpg"/>
-        <AccountButton title="Mode normal" link="/app/map" icon="/avatar-3.jpg"/>
-        <AccountButton title="Signout" link="/app/signout" icon="/avatar-3.jpg"/>
+        <AccountButton title="Déconnection" link="/signout" icon={<ArrowRightOnRectangleIcon className="w-4 h-4 inline-block"/>}/>
       </AccountButtonGroup>
 
     </Authenticated>
