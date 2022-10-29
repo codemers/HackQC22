aa0d523c8b0eebbc74767ab4e6fbc40a8254d85a
Charles-Olivier Demers <charles-olivierdemers@hotmail.com>
2022-10-29 00:00:39 -0400

    Add nav

--- app/components/Navigation/Navigation.tsx
+++ app/components/Navigation/Navigation.tsx
@@ -1,4 +1,14 @@
+import Link from "next/link";
+
 export default function Navigation() {
   // Change selon "l'app" sélectionnée
-  return <div className="bg-red-50 w-full h-[64px]">Nav</div>;
+  return (
+    <div className="bg-red-50 w-full h-[64px]">
+      <div className="flex w-full justify-between">
+        <Link href="/app/map">Map</Link>
+        <Link href="/app/me">Me</Link>
+        <Link href="/signout">signout</Link>
+      </div>
+    </div>
+  );
 }
