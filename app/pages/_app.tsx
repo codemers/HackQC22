2984f809a5d9fa03abd5c000f01de7dda0add2f3
Charles-Olivier Demers <charles-olivierdemers@hotmail.com>
2022-10-29 15:01:12 -0400

    Revert "Try to fix notch"
    This reverts commit d31395d36d5c9b12d8037bd84155f5f703b7c0f4.

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -9,15 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icon.png"></link>
         <meta name="theme-color" content="#fff" />
-        <meta
-          name="apple-mobile-web-app-status-bar-style"
-          content="black-translucent"
-        ></meta>
-        <meta
-          name="viewport"
-          content="initial-scale=1, viewport-fit=cover, width=device-width"
-        ></meta>
-        <meta name="apple-mobile-web-app-capable" content="yes"></meta>
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"></meta>
       </Head>
 
       <Component {...pageProps} />
