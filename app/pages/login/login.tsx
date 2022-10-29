3eb6726d77c97d4b17373b26743361a35afef37c
Charles-Olivier Demers <charles-olivierdemers@hotmail.com>
2022-10-28 23:24:55 -0400

    Connect to firebase

--- app/pages/login/login.tsx
+++ app/pages/login/login.tsx
@@ -1,11 +1,23 @@
+import { addDoc, collection } from "firebase/firestore";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import { database, app } from "../utils/firebaseConfig";
 
 export default function Login() {
   return (
     <div>
       <h1>Login</h1>
+      <button
+        onClick={async () => {
+          const dbInstance = collection(database, "notes");
+          addDoc(dbInstance, {
+            test: "test",
+          });
+        }}
+      >
+        Test
+      </button>
     </div>
   );
 }
