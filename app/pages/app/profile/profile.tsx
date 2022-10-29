4d0a098dc598c5ce117201e2996db2140b01f658
Dereck Pouliot <dereckmpouliot@gmail.com>
2022-10-29 16:48:22 -0400

    Fix rebase fuckup

--- app/pages/app/profile/profile.tsx
+++ app/pages/app/profile/profile.tsx
@@ -11,34 +11,54 @@ import AccountButton from "../../../components/AccountButton/AccountButton";
 import AccountButtonGroup from "../../../components/AccountButtonGroup/AccountButtonGroup";
 
 import terminalImage from "../../../public/images/profile/ma-borne-white.png";
+import { getAuth, signOut } from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { app, database } from "../../../utils/firebaseConfig";
+import { useEffect, useState } from "react";
+import { doc, getDoc } from "firebase/firestore"; 
 
 export default function Profile() {
+  const auth = getAuth(app);
+  const [user, loading, error] = useAuthState(auth);
+
+  const [profile, setProfile] = useState<any>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if(!user) return;
+      const usersRef = doc(database, "users", user.uid);
+      const docSnap = await getDoc(usersRef);
+
+      if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data());
+        setProfile(docSnap.data());
+
+      } else {
+        console.log("No such document!");
+      }
+    }
+  
+    fetchData()
+      .catch(console.error);
+  }, [user])
+
+  console.log("profile", profile)
+
   return (
     <Authenticated className="m-auto">
       <p className="text-center m-4">
-        <img
-          src="/avatar-3.jpg"
-          alt=""
-          className="inline-block h-10 w-10 mr-4 flex-none rounded-full"
-        />
-        Profile
+        <img src={profile?.photoUrl} alt=""/>
+        {profile?.name}
       </p>
 
       <AccountButtonGroup>
-        <AccountButton title="Messagerie" link="/app/messages" icon={<InboxIcon className="w-4 h-4 inline-block" />}/>
-      </AccountButtonGroup>
-
-      <AccountButtonGroup>
         <AccountButton title="Méthodes de paiement" link="/app/payment" icon={<CreditCardIcon className="w-4 h-4 inline-block" />}/>
-        <AccountButton title="Paiements auto." link="/app/payment-auto" icon={<ArrowPathIcon className="w-4 h-4 inline-block" />}/>
-        <AccountButton title="Historique des transactions" link="/app/transactions" icon={<ClockIcon className="w-4 h-4 inline-block" />}/>
       </AccountButtonGroup>
-
       <AccountButtonGroup>
         <AccountButton title="Offrir un cadeau" link="/app/gift" icon={<GiftIcon className="w-4 h-4 inline-block" />}/>
         <AccountButton title="Saisir un code recu" link="/app/gift-code" icon={<GiftIcon className="w-4 h-4 inline-block" />}/>
       </AccountButtonGroup>
-
+      
       <AccountButtonGroup>
         <AccountButton title="Carte de membre" link="/app/transactions" icon={<CreditCardIcon className="w-4 h-4 inline-block" />}/>
       </AccountButtonGroup>
