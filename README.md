0b2c999c0a19b77c603e3e5848fc76d3679d8c44
Justin Brûlotte <justin.brlotte797@gmail.com>
2022-10-29 13:34:26 -0400

    Adding Firebase function for OnCreate user
    Signed-off-by: Justin Brûlotte <justin.brlotte797@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # HackQC22
+
+
+## Deploy Firebase Functions
+```bash
+firebase deploy --only functions
+```
+
+## Emulator
+```bash
+firebase emulators:start
+```
---
+++ functions/index.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-await-in-loop */
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+exports.initializeUserOnCreate = functions.auth.user().onCreate(async (firebaseUser) => {
+  const userId = firebaseUser.providerData[0].uid
+
+  return admin.firestore().collection("users").doc(firebaseUser.uid).set({
+    email: firebaseUser.email,
+    name: firebaseUser.displayName,
+    photoUrl: firebaseUser.photoURL,
+    emailVerified: firebaseUser.emailVerified,
+    phoneNumber: firebaseUser.phoneNumber,
+    providerId: firebaseUser.providerData[0].providerId,
+    userId: userId,
+    createdAt: admin.firestore.FieldValue.serverTimestamp(),
+  }, { ignoreUndefinedProperties: true } );
+});
+
+exports.initializeUserOnDelete = functions.auth.user().onDelete(async (firebaseUser) => {
+  const doc = admin.firestore().collection("users").doc(firebaseUser.uid);
+  return doc.delete();
+})
