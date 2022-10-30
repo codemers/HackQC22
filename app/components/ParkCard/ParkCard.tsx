2ba112ff42b36ef1c8b4162434fa2dbc092be654
Charles-Olivier Demers <charles-olivierdemers@hotmail.com>
2022-10-30 12:35:18 -0400

    update terminal card preview

---
+++ app/components/ParkCard/ParkCard.tsx
@@ -0,0 +1,105 @@
+import cx from "classix";
+import { useState } from "react";
+
+import XMarkIcon from "@heroicons/react/20/solid/XMarkIcon";
+import { Park } from "../../pages/app/map/map";
+import { ArrowUpRightIcon } from "@heroicons/react/20/solid";
+
+type Props = {
+  onClose: () => void;
+  park: Park;
+};
+
+export default function ParkCard(props: Props) {
+  const { park } = props;
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const terminals = park.terminals;
+  const parkAvailable = terminals.some((t) => t.available);
+
+  return (
+    <div>
+      {!isExpanded && (
+        <img
+          src={"/images/map/image-terminal.png"}
+          className="w-full h-32 rounded-t-2xl object-cover"
+        />
+      )}
+      <div
+        className={cx("bg-white p-4 flex flex-col items-start border-b")}
+        style={{ height: isExpanded ? "calc(100vh - 64px)" : "128" }}
+        onClick={() => setIsExpanded(!isExpanded)}
+      >
+        {!isExpanded && (
+          <div className="absolute top-24 right-5 mt-2">
+            <button className="bg-[#50b0C6] p-2 rounded-full">
+              <ArrowUpRightIcon className="w-8 h-8 text-white" />
+            </button>
+          </div>
+        )}
+        <div className="flex w-full justify-center">
+          <button
+            onClick={props.onClose}
+            className="bg-gray-300 w-6 h-1 rounded-md"
+          ></button>
+        </div>
+        <div className="flex justify-between w-full">
+          <h1>{park.parkName}</h1>
+        </div>
+        <div className="flex w-full">
+          <svg
+            className="w-5 mr-2"
+            viewBox="0 0 139 101"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M57.7029 35.6009C66.6747 20.7113 78.1485 16.2921 85.9182 17.1367C90.5867 17.6439 94.0216 20.0258 95.2097 23.464C96.5323 27.2572 95.173 32.2204 91.5756 37.5595L91.5546 37.5508C81.9433 51.5904 65.5338 58.6766 52.1277 58.3583C51.5861 51.4855 53.4206 43.7294 57.7012 35.6009M136.207 63.9441C132.716 60.7787 127.354 61.041 124.192 64.5247C112.196 77.7598 99.6336 82.1162 91.2104 83.4278C80.8811 85.0594 70.702 82.9871 63.3079 77.7703C56.5096 72.9908 52.7514 66.263 52.1259 58.3706C57.6121 62.9035 71.5127 61.2107 82.6352 57.0065C82.9235 57.4612 83.2537 57.8967 83.6364 58.3006C85.2962 60.1002 87.564 61.0008 89.8319 61.0008C91.9163 61.0008 94.0129 60.2261 95.6605 58.6854C109.807 45.3593 115.542 30.5572 111.348 18.0479C108.078 8.26494 99.0308 1.42528 87.751 0.2046C79.9201 -0.652326 71.7346 1.19444 64.0976 5.52103C55.8282 10.2079 48.7207 17.4515 42.9864 27.0369C42.9445 27.0998 42.9235 27.168 42.8798 27.2257C42.8449 27.2887 42.803 27.3481 42.7715 27.4094C35.9575 40.2527 33.5604 53.1853 35.8335 64.8028C37.986 75.6385 44.2549 85.2029 53.5114 91.7172C62.132 97.7717 73.0327 101 84.4086 101C87.5308 101 90.681 100.741 93.8434 100.255C109.807 97.7507 124.66 89.3633 136.789 75.969C139.952 72.4959 139.689 67.1025 136.207 63.9441Z"
+              fill="#00A0B4"
+            />
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M7.05467 24.1034C11.52 23.4984 15.8165 25.4895 16.8854 31.6476C18.7203 42.2805 26.5003 51.2524 38.2025 55.5646C42.7044 57.229 47.4082 58.069 52.133 58.1261H52.1417C64.1015 60.9329 82.3407 57.2964 96.6751 47.5088C102.73 43.3607 108.109 38.1272 112 31.8015C110.357 40.6057 104.746 49.8455 95.5261 58.4475L95.3607 58.5823C95.3137 58.6255 95.2737 58.6688 95.2284 58.7241C82.6366 69.4036 67.6564 75 52.7457 75C45.8345 75 38.9406 73.7953 32.2939 71.348C14.7337 64.8736 2.71466 50.9793 0.122497 34.2039C-0.887212 27.2282 4.60526 24.4248 7.05467 24.1034Z"
+              fill="#87B925"
+            />
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M136.207 63.926C132.716 60.7591 127.354 61.0216 124.192 64.5069C112.194 77.7503 99.6334 82.1087 91.2102 83.4192C80.8809 85.0517 70.7019 82.9783 63.3078 77.759C50.9553 69.0788 48.635 53.9634 56.9044 37.1105L57.0092 36.9005C57.2434 36.4631 57.4705 36.0204 57.7029 35.569C66.6746 20.6722 78.1483 16.2508 85.9163 17.0959C87.8609 17.3058 89.5888 17.8482 91.0338 18.6408L91.0442 18.6356C84.6845 12.2702 58.3371 13.2291 43.217 29.9L43.1961 29.8808C41.7686 31.4502 40.4408 33.1562 39.2457 35.0056V35.0143C35.1887 45.3025 34.0129 55.4646 35.8317 64.7869C37.986 75.6279 44.2531 85.1951 53.5096 91.7127C62.1337 97.7701 73.0326 101 84.4102 101C87.5324 101 90.6808 100.743 93.845 100.255C109.809 97.7491 124.66 89.3576 136.791 75.9568C139.953 72.482 139.687 67.086 136.207 63.926Z"
+              fill="#133B62"
+            />
+          </svg>
+
+          <div className="flex justify-between w-full">
+            <h2 className="text-gray-400">{`${park.city}`} </h2>
+            <div>
+              <span className="text-[#8ABF55] text-lg">{`${
+                terminals.filter((t) => t.available).length
+              }`}</span>
+              <span className="text-gray-300 text-sm">{`/${terminals.length}`}</span>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-between w-full">
+          <div
+            className={cx(
+              "px-2 rounded-md mt-2",
+              parkAvailable ? "bg-[#8ABF55]" : "bg-red-500"
+            )}
+          >
+            <span className="text-white text-sm">
+              {parkAvailable ? "Disponible" : "Indisponible"}
+            </span>
+          </div>
+
+          <div className="px-2 border h-7 rounded-md">
+            <span className="text-black text-sm">{terminals[0].type}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/pages/app/map/map.tsx
+++ app/pages/app/map/map.tsx
@@ -10,7 +10,17 @@ import InformationCircleIcon from "@heroicons/react/20/solid/InformationCircleIc
 import AdjustmentsHorizontalIcon from "@heroicons/react/20/solid/AdjustmentsHorizontalIcon";
 import ArrowUpRightIcon from "@heroicons/react/20/solid/ArrowUpRightIcon";
 import cx from "classix";
-import TerminalCard from "../../../components/TerminalCard/TerminalCard";
+import ParkCard from "../../../components/ParkCard/ParkCard";
+
+export type Park = {
+  id: string;
+  parkName: string;
+  lng: number;
+  lat: number;
+  state: State;
+  terminals: { name: string; available: boolean; type: string }[];
+  city: string;
+};
 
 function LocationPin({ lng, lat }: { lng: number; lat: number }) {
   return (
@@ -35,6 +45,7 @@ function TerminalPin({
   zoom,
   onClick,
   state,
+  park,
 }: {
   id: string;
   lng: number;
@@ -42,6 +53,7 @@ function TerminalPin({
   zoom: number;
   onClick: (id: string) => void;
   state: State;
+  park?: Park;
 }) {
   if (zoom <= 14) {
     return (
@@ -67,40 +79,73 @@ function TerminalPin({
       lat={lat}
       lng={lng}
       className={cx(
-        "rounded-md flex items-center px-2 w-20 h-10",
-        state === "private" ? "bg-pink-500" : "bg-green-500"
+        "rounded-md flex items-center justify-between px-2 w-20 h-10 border-white border",
+        state === "private" ? "bg-pink-500" : "bg-[#8ABF55]"
       )}
       onClick={() => onClick(id)}
     >
-      <div className="w-4 h-4 bg-gray-500 "></div>
+      <div className="w-6 h-6 flex items-center justify-center bg-white rounded-md">
+        <svg
+          className="w-4"
+          viewBox="0 0 139 101"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M57.7029 35.6009C66.6747 20.7113 78.1485 16.2921 85.9182 17.1367C90.5867 17.6439 94.0216 20.0258 95.2097 23.464C96.5323 27.2572 95.173 32.2204 91.5756 37.5595L91.5546 37.5508C81.9433 51.5904 65.5338 58.6766 52.1277 58.3583C51.5861 51.4855 53.4206 43.7294 57.7012 35.6009M136.207 63.9441C132.716 60.7787 127.354 61.041 124.192 64.5247C112.196 77.7598 99.6336 82.1162 91.2104 83.4278C80.8811 85.0594 70.702 82.9871 63.3079 77.7703C56.5096 72.9908 52.7514 66.263 52.1259 58.3706C57.6121 62.9035 71.5127 61.2107 82.6352 57.0065C82.9235 57.4612 83.2537 57.8967 83.6364 58.3006C85.2962 60.1002 87.564 61.0008 89.8319 61.0008C91.9163 61.0008 94.0129 60.2261 95.6605 58.6854C109.807 45.3593 115.542 30.5572 111.348 18.0479C108.078 8.26494 99.0308 1.42528 87.751 0.2046C79.9201 -0.652326 71.7346 1.19444 64.0976 5.52103C55.8282 10.2079 48.7207 17.4515 42.9864 27.0369C42.9445 27.0998 42.9235 27.168 42.8798 27.2257C42.8449 27.2887 42.803 27.3481 42.7715 27.4094C35.9575 40.2527 33.5604 53.1853 35.8335 64.8028C37.986 75.6385 44.2549 85.2029 53.5114 91.7172C62.132 97.7717 73.0327 101 84.4086 101C87.5308 101 90.681 100.741 93.8434 100.255C109.807 97.7507 124.66 89.3633 136.789 75.969C139.952 72.4959 139.689 67.1025 136.207 63.9441Z"
+            fill="#00A0B4"
+          />
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M7.05467 24.1034C11.52 23.4984 15.8165 25.4895 16.8854 31.6476C18.7203 42.2805 26.5003 51.2524 38.2025 55.5646C42.7044 57.229 47.4082 58.069 52.133 58.1261H52.1417C64.1015 60.9329 82.3407 57.2964 96.6751 47.5088C102.73 43.3607 108.109 38.1272 112 31.8015C110.357 40.6057 104.746 49.8455 95.5261 58.4475L95.3607 58.5823C95.3137 58.6255 95.2737 58.6688 95.2284 58.7241C82.6366 69.4036 67.6564 75 52.7457 75C45.8345 75 38.9406 73.7953 32.2939 71.348C14.7337 64.8736 2.71466 50.9793 0.122497 34.2039C-0.887212 27.2282 4.60526 24.4248 7.05467 24.1034Z"
+            fill="#87B925"
+          />
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M136.207 63.926C132.716 60.7591 127.354 61.0216 124.192 64.5069C112.194 77.7503 99.6334 82.1087 91.2102 83.4192C80.8809 85.0517 70.7019 82.9783 63.3078 77.759C50.9553 69.0788 48.635 53.9634 56.9044 37.1105L57.0092 36.9005C57.2434 36.4631 57.4705 36.0204 57.7029 35.569C66.6746 20.6722 78.1483 16.2508 85.9163 17.0959C87.8609 17.3058 89.5888 17.8482 91.0338 18.6408L91.0442 18.6356C84.6845 12.2702 58.3371 13.2291 43.217 29.9L43.1961 29.8808C41.7686 31.4502 40.4408 33.1562 39.2457 35.0056V35.0143C35.1887 45.3025 34.0129 55.4646 35.8317 64.7869C37.986 75.6279 44.2531 85.1951 53.5096 91.7127C62.1337 97.7701 73.0326 101 84.4102 101C87.5324 101 90.6808 100.743 93.845 100.255C109.809 97.7491 124.66 89.3576 136.791 75.9568C139.953 72.482 139.687 67.086 136.207 63.926Z"
+            fill="#133B62"
+          />
+        </svg>
+      </div>
+
+      <div className="font-bold flex items-start">
+        <span className="text-white text-xl">
+          {park?.terminals.filter((t) => t.available).length}
+        </span>
+        <span className="text-gray-300 mt-1"> / {park?.terminals.length}</span>
+      </div>
     </button>
   );
 }
 
 export default function Map() {
   const geolocation = useGeolocation();
-  const [zoom, setZoom] = useState(11);
+  const [zoom, setZoom] = useState(18);
   const [bounds, setBounds] = useState(null);
   const mapRef = useRef();
 
-  const [selectedTerminal, setSelectedTerminal] = useState<string>();
-  const [terminals, setTerminals] = useState([
+  const [selectedPark, setSelectedPark] = useState<string>();
+  const [parks, setParks] = useState<Park[]>([
     {
-      lat: 45.42771818115786,
+      id: "Mon=-id",
+      lat: 45.32771818115786,
       lng: -72.52708030990577,
-      id: "123",
+      parkName: "Parc de la Gatineau",
       state: "public" as State,
-    },
-    {
-      lat: 45.6074819,
-      lng: -73.5865146,
-      id: "1234",
-      state: "private" as State,
+      city: "Orford",
+      terminals: [
+        { name: "CAE-345", available: true, type: "Niveau 2" },
+        { name: "CAE-346", available: false, type: "Niveau 1" },
+      ],
     },
   ]);
 
   const { clusters, supercluster } = useSupercluster({
-    points: terminals.map((t) => {
+    points: parks.map((t) => {
       return {
         type: t.state,
         id: t.id,
@@ -172,9 +217,7 @@ export default function Map() {
           options={{ zoomControl: false, fullscreenControl: false }}
           zoom={zoom}
           yesIWantToUseGoogleMapApiInternals
-          onClick={() =>
-            selectedTerminal ? setSelectedTerminal(undefined) : () => {}
-          }
+          onClick={() => (selectedPark ? setSelectedPark(undefined) : () => {})}
           onGoogleApiLoaded={({ map }) => {
             mapRef.current = map;
           }}
@@ -196,7 +239,7 @@ export default function Map() {
             const [longitude, latitude] = cluster.geometry.coordinates;
             const { cluster: isCluster, point_count: pointCount } =
               cluster.properties;
-
+            console.log(cluster.id);
             if (isCluster) {
               // @ts-ignore
               return (
@@ -221,16 +264,20 @@ export default function Map() {
                 lng={longitude}
                 lat={latitude}
                 zoom={zoom}
-                onClick={setSelectedTerminal}
+                onClick={setSelectedPark}
                 state={cluster.type}
+                park={parks.find((p) => p.id === cluster.id)}
               />
             );
           })}
         </GoogleMapReact>
 
-        {selectedTerminal && (
+        {selectedPark && parks.find((p) => p.id === selectedPark) && (
           <div className="absolute bottom-[64px] w-full z-50">
-            <TerminalCard onClose={() => setSelectedTerminal(undefined)} />
+            <ParkCard
+              onClose={() => setSelectedPark(undefined)}
+              park={parks.find((p) => p.id === selectedPark) as Park}
+            />
           </div>
         )}
       </div>
