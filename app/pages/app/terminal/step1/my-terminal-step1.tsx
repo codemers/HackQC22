da8fe59d3abd384aa1403fd4229f4e8deb67cdbe
Justin Brûlotte <justin.brlotte797@gmail.com>
2022-10-29 23:08:21 -0400

    Adding Step1 to Step 4 for terminal unbording
    Signed-off-by: Justin Brûlotte <justin.brlotte797@gmail.com>

--- app/pages/app/terminal/step1/my-terminal-step1.tsx
+++ app/pages/app/terminal/step1/my-terminal-step1.tsx
@@ -1,20 +1,26 @@
+import Link from "next/link";
 import { XCircleIcon, ArrowLongRightIcon } from "@heroicons/react/20/solid";
 
-export default function Me() {
+export default function TerminalStep1() {
   return (
     <div className="h-screen bg-my-terminal-background bg-cover bg-center">
       <div className="h-4/6">
         <div className="grid grid-cols-2 p-4">
-          <a href="/app/profile">
+          <Link href="/app/profile">
             <XCircleIcon className="h-10 block float-left" />
-          </a>
+          </Link>
         </div>
       </div>
 
       <div className="h-2/6 bg-white p-6">
         <h1 className="text-3xl mb-6">Louez votre borne de recharge !</h1>
         <p className="text-base mb-8">Aidez-nous à augmenter la capacité du réseau de Circuit Électrique.</p>
-        <button className="w-full align-middle h-14 border solid rounded-3xl bg-[#02B3C9] text-white text-center"><span className="mr-2">Allons-y</span><ArrowLongRightIcon className="h-7 inline-block" /></button>
+        <Link href="/app/terminal/step2">
+          <button className="w-full align-middle h-14 border solid rounded-3xl bg-[#02B3C9] text-white text-center">
+            <span className="mr-2">Allons-y</span>
+            <ArrowLongRightIcon className="h-7 inline-block" />
+          </button>
+        </Link>
       </div>
     </div>
   );
